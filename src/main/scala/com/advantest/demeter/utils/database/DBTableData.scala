5d25f254e5bcecc0fa14e2a94ec5b1422b3ad98a
mengen.dai <mengen.dai@outlook.com>
2024-10-13 22:37:41 +0800

    将 DBTableData 类中的成员变量改为方法
    将 DBTableData 类中的成员变量（id, creatorId, updaterId, createDateTime, updateDateTime）改为方法，因为在 Slick 中，使用 def 定义的字段可以更好地与 Slick 的查询和映射机制结合。

--- src/main/scala/com/advantest/demeter/utils/database/DBTableData.scala
+++ src/main/scala/com/advantest/demeter/utils/database/DBTableData.scala
@@ -17,29 +17,29 @@ trait DBTableData {
    * Unique identifier of the record.
    * @return A [[Rep[Long]]] representing the ID of the record.
    */
-  val id: Rep[Long]
+  def id: Rep[Long]
 
   /**
    * Identifier of the user who created the record.
    * @return A [[Rep[Long]]] representing the creator's ID.
    */
-  val creatorId: Rep[Long]
+  def creatorId: Rep[Long]
 
   /**
    * Identifier of the user who last updated the record.
    * @return A [[Rep[Long]]] representing the updater's ID.
    */
-  val updaterId: Rep[Long]
+  def updaterId: Rep[Long]
 
   /**
    * Timestamp when the record was created.
    * @return A [[Rep[LocalDateTime]]] representing the creation time.
    */
-  val createDateTime: Rep[LocalDateTime]
+  def createDateTime: Rep[LocalDateTime]
 
   /**
    * Timestamp when the record was last updated.
    * @return A [[Rep[LocalDateTime]]] representing the last update time.
    */
-  val updateDateTime: Rep[LocalDateTime]
+  def updateDateTime(): Rep[LocalDateTime]
 }
