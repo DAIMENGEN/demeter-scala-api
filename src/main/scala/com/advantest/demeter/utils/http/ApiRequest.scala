ed9e1e455ab1db925cab75c755dc75bf70fb486c
mengen.dai <mengen.dai@outlook.com>
2024-10-26 10:56:27 +0800

    将 RichRequestParams 类移动到 ApiRequest 中
    - 从 HttpRoute 中移除 RichRequestParams 类
    - 在 ApiRequest 中添加 RichRequestParams 类

--- src/main/scala/com/advantest/demeter/utils/http/ApiRequest.scala
+++ src/main/scala/com/advantest/demeter/utils/http/ApiRequest.scala
@@ -2,8 +2,10 @@ package com.advantest.demeter.utils.http
 
 import akka.http.scaladsl.server.{Directive0, Directive1}
 import com.advantest.demeter.DemeterScalaApi.{DEMETER_EXECUTION_CONTEXT, DEMETER_SESSION_MANAGER, REFRESH_TOKEN_STORAGE}
+import com.advantest.demeter.utils.serialize.format.reader.JsonReaderFormat
 import com.softwaremill.session.SessionDirectives._
 import com.softwaremill.session.SessionOptions._
+import spray.json.{JsValue, JsonFormat}
 
 /**
  * Create on 2024/10/25
@@ -12,6 +14,32 @@ import com.softwaremill.session.SessionOptions._
  * Trait representing an API request with session management capabilities.
  */
 trait ApiRequest {
+
+  type HttpRequestParams = Map[String, JsValue]
+
+  implicit class RichRequestParams(params: HttpRequestParams) {
+    private lazy val reader: JsonReaderFormat = JsonReaderFormat(params)
+
+    def read[T: JsonFormat](key: String): T = reader.read[T](key)
+
+    def readInt(key: String): Int = reader.readInt(key)
+
+    def readOptionInt(key: String): Option[Int] = reader.readOptionInt(key)
+
+    def readLong(key: String): Long = reader.readLong(key)
+
+    def readLongArray(key: String): Seq[Long] = reader.readLongArray(key)
+
+    def readOptionLong(key: String): Option[Long] = reader.readOptionLong(key)
+
+    def readString(key: String): String = reader.readString(key)
+
+    def readOptionString(key: String): Option[String] = reader.readOptionString(key)
+
+    def readBoolean(key: String): Boolean = reader.readBoolean(key)
+
+    def readOptionBoolean(key: String): Option[Boolean] = reader.readOptionBoolean(key)
+  }
 
   /**
    * A directive that requires a session to be present in the request.
--- src/main/scala/com/advantest/demeter/utils/http/HttpRoute.scala
+++ src/main/scala/com/advantest/demeter/utils/http/HttpRoute.scala
@@ -4,8 +4,6 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives.concat
 import akka.http.scaladsl.server.Route
 import com.advantest.demeter.DemeterScalaApi.{DEMETER_EXECUTION_CONTEXT, DEMETER_SYSTEM}
-import com.advantest.demeter.utils.serialize.format.reader.JsonReaderFormat
-import spray.json.{JsValue, JsonFormat}
 
 import scala.util.{Failure, Success}
 
@@ -14,33 +12,7 @@ import scala.util.{Failure, Success}
  * Author: mengen.dai@outlook.com
  */
 trait HttpRoute {
-  type HttpRequestParams = Map[String, JsValue]
-
   def route: Route
-
-  implicit class RichRequestParams(params: HttpRequestParams) {
-    private lazy val reader: JsonReaderFormat = JsonReaderFormat(params)
-
-    def read[T: JsonFormat](key: String): T = reader.read[T](key)
-
-    def readInt(key: String): Int = reader.readInt(key)
-
-    def readOptionInt(key: String): Option[Int] = reader.readOptionInt(key)
-
-    def readLong(key: String): Long = reader.readLong(key)
-
-    def readLongArray(key: String): Seq[Long] = reader.readLongArray(key)
-
-    def readOptionLong(key: String): Option[Long] = reader.readOptionLong(key)
-
-    def readString(key: String): String = reader.readString(key)
-
-    def readOptionString(key: String): Option[String] = reader.readOptionString(key)
-
-    def readBoolean(key: String): Boolean = reader.readBoolean(key)
-
-    def readOptionBoolean(key: String): Option[Boolean] = reader.readOptionBoolean(key)
-  }
 }
 
 object HttpRoute {
