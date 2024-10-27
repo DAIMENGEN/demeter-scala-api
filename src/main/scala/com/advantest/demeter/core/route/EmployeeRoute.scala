9abb23f888ecf3de899646481309cb0a58534dc6
mengen.dai <mengen.dai@outlook.com>
2024-10-27 17:05:12 +0800

    添加员工相关路由并优化代码
    - 新增注册、批量注册、重置密码和获取用户名路由
    - 修正员工服务实例变量名称

--- src/main/scala/com/advantest/demeter/core/route/EmployeeRoute.scala
+++ src/main/scala/com/advantest/demeter/core/route/EmployeeRoute.scala
@@ -3,6 +3,7 @@ package com.advantest.demeter.core.route
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import com.advantest.demeter.DemeterScalaApi.DEMETER_EXECUTION_CONTEXT
+import com.advantest.demeter.core.entity.EmployeeEntity
 import com.advantest.demeter.core.service.EmployeeService
 import com.advantest.demeter.utils.http.{ApiRequest, ApiResponse, HttpRoute}
 import spray.json.DefaultJsonProtocol._
@@ -14,11 +15,15 @@ import scala.concurrent.Future
  * Author: mengen.dai@outlook.com
  */
 case class EmployeeRoute() extends HttpRoute with ApiRequest with ApiResponse {
-  private val userService = EmployeeService()
+  private val employeeService = EmployeeService()
 
   override def route: Route = concat(
     loginRoute,
     logoutRoute,
+    registerRoute,
+    batchRegisterRoute,
+    resetPasswordRoute,
+    getUsernameRoute,
   )
 
   private def loginRoute: Route = path("loginRoute") {
@@ -27,7 +32,7 @@ case class EmployeeRoute() extends HttpRoute with ApiRequest with ApiResponse {
         request =>
           val account = request.readString("account")
           val password = request.readString("password")
-          val future = userService.verifyPassword(account, password).map(generateToken)
+          val future = employeeService.verifyPassword(account, password).map(generateToken)
           response(future)
       }
     }
@@ -45,6 +50,51 @@ case class EmployeeRoute() extends HttpRoute with ApiRequest with ApiResponse {
             response(Future("Logout failure"))
           }
         }
+      }
+    }
+  }
+
+  private def registerRoute: Route = path("registerRoute") {
+    post {
+      entity(as[EmployeeEntity]) {
+        request =>
+          val future = employeeService.register(request);
+          response(future)
+      }
+    }
+  }
+
+  private def batchRegisterRoute: Route = path("batchRegisterRoute") {
+    post {
+      validateToken { employee =>
+        entity(as[List[EmployeeEntity]]) {
+          request =>
+            val future = employeeService.register(employee.id, request);
+            response(future)
+        }
+      }
+    }
+  }
+
+  private def resetPasswordRoute: Route = path("resetPasswordRoute") {
+    post {
+      validateToken { employee =>
+        entity(as[HttpRequestParams]) {
+          request =>
+            val oldPassword = request.readString("oldPassword")
+            val newPassword = request.readString("newPassword")
+            val future = employeeService.resetPassword(employee.id, oldPassword, newPassword)
+            response(future)
+        }
+      }
+    }
+  }
+
+  private def getUsernameRoute: Route = path("getUsernameRoute") {
+    get {
+      validateToken { employee =>
+        val username = employee.username
+        response(Future(username))
       }
     }
   }
