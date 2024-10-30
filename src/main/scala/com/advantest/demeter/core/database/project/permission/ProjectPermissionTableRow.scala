4638ba3c76c3ebe630b87306fa1d6a8ee4629252
mengen.dai <mengen.dai@outlook.com>
2024-10-30 22:52:14 +0800

    新增项目权限相关实体和表结构
    - 添加项目权限类别和权限定义
    - 创建项目权限实体类和数据库表结构
    - 实现项目权限相关的数据库操作方法

---
+++ src/main/scala/com/advantest/demeter/core/database/project/permission/ProjectPermissionTableRow.scala
@@ -0,0 +1,32 @@
+package com.advantest.demeter.core.database.project.permission
+
+import com.advantest.demeter.core.constant.project.permission.{ProjectPermission => Permission, ProjectPermissionCategory => Category}
+import com.advantest.demeter.core.entity.project.permission.ProjectPermissionEntity
+import com.advantest.demeter.utils.database.DBTableRow
+
+import java.time.LocalDateTime
+
+/**
+ * Create on 2024/10/30
+ * Author: mengen.dai@outlook.com
+ */
+final case class ProjectPermissionTableRow(
+                                            id: Long,
+                                            category: Category,
+                                            permission: Permission,
+                                            employeeId: Long,
+                                            projectId: Long,
+                                            creatorId: Long,
+                                            updaterId: Long,
+                                            createDateTime: LocalDateTime = LocalDateTime.now(),
+                                            updateDateTime: LocalDateTime = LocalDateTime.now()
+                                          ) extends DBTableRow {
+  override def toString: String = s"ProjectPermissionTableRow(id=$id, category=$category, permission=$permission, employeeId=$employeeId, projectId=$projectId, creatorId=$creatorId, updaterId=$updaterId, createDateTime=$createDateTime, updateDateTime=$updateDateTime)"
+
+  def toEntity: ProjectPermissionEntity = ProjectPermissionEntity(
+    id = id,
+    category = category,
+    permission = permission,
+    employeeId = employeeId,
+  )
+}
---
+++ src/main/scala/com/advantest/demeter/core/database/project/permission/ProjectPermissionTableSchema.scala
@@ -0,0 +1,52 @@
+package com.advantest.demeter.core.database.project.permission
+
+import com.advantest.demeter.core.constant.project.permission.{ProjectPermission, ProjectPermissionCategory}
+import com.advantest.demeter.core.database.project.{ProjectTableRow, ProjectTableSchema}
+import com.advantest.demeter.utils.database.DBTableSchema
+import slick.jdbc.MySQLProfile.api._
+import slick.lifted.{ForeignKeyQuery, ProvenShape}
+
+import java.time.LocalDateTime
+
+/**
+ * Create on 2024/10/30
+ * Author: mengen.dai@outlook.com
+ */
+final class ProjectPermissionTableSchema(tag: Tag) extends Table[ProjectPermissionTableRow](tag, "PROJECT_PERMISSION_TABLE") with DBTableSchema {
+
+  private type Permission = ProjectPermission
+
+  private type Category = ProjectPermissionCategory
+
+  override def id: Rep[Long] = column[Long]("ID", O.PrimaryKey, O.Unique)
+
+  def category: Rep[Category] = column[Category]("CATEGORY")
+
+  def permission: Rep[Permission] = column[Permission]("PERMISSION")
+
+  def employeeId: Rep[Long] = column[Long]("EMPLOYEE_ID")
+
+  def projectId: Rep[Long] = column[Long]("PROJECT_ID")
+
+  override def creatorId: Rep[Long] = column[Long]("CREATOR_ID")
+
+  override def updaterId: Rep[Long] = column[Long]("UPDATER_ID")
+
+  override def createDateTime: Rep[LocalDateTime] = column[LocalDateTime]("CREATE_DATE_TIME", O.Default(LocalDateTime.now()))
+
+  override def updateDateTime(): Rep[LocalDateTime] = column[LocalDateTime]("UPDATE_DATE_TIME", O.Default(LocalDateTime.now()))
+
+  def projectIdFk: ForeignKeyQuery[ProjectTableSchema, ProjectTableRow] = foreignKey("PROJECT_ID_FK", projectId, TableQuery[ProjectTableSchema])(_.id, onDelete = ForeignKeyAction.Cascade)
+
+  override def * : ProvenShape[ProjectPermissionTableRow] = (
+    id,
+    category,
+    permission,
+    employeeId,
+    projectId,
+    creatorId,
+    updaterId,
+    createDateTime,
+    updateDateTime()
+  ) <> ((ProjectPermissionTableRow.apply _).tupled, ProjectPermissionTableRow.unapply)
+}
