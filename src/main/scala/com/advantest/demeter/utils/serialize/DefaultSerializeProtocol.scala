172eb977fd24d857210d13cdfc0182ddedf72625
mengen.dai <mengen.dai@outlook.com>
2024-11-09 19:51:12 +0800

    优化日期和长整型的序列化与反序列化
    - 为 Long 类型添加自定义 JsonFormat，支持字符串和数字格式
    - 优化 LocalDate 和 LocalDateTime 的 JsonFormat 实现
    - 改进错误处理，提高反序列化失败时的错误信息可读性

--- src/main/scala/com/advantest/demeter/utils/serialize/DefaultSerializeProtocol.scala
+++ src/main/scala/com/advantest/demeter/utils/serialize/DefaultSerializeProtocol.scala
@@ -2,7 +2,7 @@ package com.advantest.demeter.utils.serialize
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import com.advantest.demeter.utils.date.DateFormatterUtil
-import spray.json.{JsString, JsValue, RootJsonFormat}
+import spray.json.{JsNumber, JsString, JsValue, JsonFormat}
 
 import java.time.{LocalDate, LocalDateTime}
 
@@ -16,31 +16,57 @@ import java.time.{LocalDate, LocalDateTime}
 trait DefaultSerializeProtocol extends SprayJsonSupport {
 
   /**
+   * Implicit JSON format for `Long`.
+   * Provides methods to serialize and deserialize `Long` values to and from JSON strings.
+   */
+  implicit object LongJsonFormat extends JsonFormat[Long] {
+
+    /**
+     * Serializes a `Long` value to a JSON string.
+     *
+     * @param obj The `Long` value to be serialized.
+     * @return A JSON string representing the `Long` value.
+     */
+    override def write(obj: Long): JsValue = JsString(obj.toString)
+
+    /**
+     * Deserializes a JSON string to a `Long` value.
+     *
+     * @param json The JSON value to be deserialized.
+     * @return A `Long` value.
+     * @throws IllegalArgumentException if the JSON value is not a string or a number.
+     */
+    override def read(json: JsValue): Long = json match {
+      case JsString(value) => value.toLong
+      case JsNumber(value) => value.toLong
+      case _ => throw new IllegalArgumentException(s"Expected a string as a Long, but received ${json.getClass.getSimpleName}.")
+    }
+  }
+
+  /**
    * Implicit JSON format for `LocalDate`.
    * Provides methods to serialize and deserialize `LocalDate` objects to and from JSON strings.
    */
-  implicit val localDateFormat: RootJsonFormat[LocalDate] = new RootJsonFormat[LocalDate] {
+  implicit object LocalDateJsonFormat extends JsonFormat[LocalDate] {
 
     /**
      * Serializes a `LocalDate` object to a JSON string.
      *
-     * @param date The `LocalDate` object to be serialized
-     * @return A JSON string representing the `LocalDate` object
+     * @param obj The `LocalDate` object to be serialized.
+     * @return A JSON string representing the `LocalDate` object.
      */
-    override def write(date: LocalDate): JsValue = {
-      JsString(DateFormatterUtil.formatLocalDate(date))
-    }
+    override def write(obj: LocalDate): JsValue = JsString(DateFormatterUtil.formatLocalDate(obj))
 
     /**
      * Deserializes a JSON string to a `LocalDate` object.
      *
-     * @param json The JSON value to be deserialized
-     * @return A `LocalDate` object
-     * @throws IllegalArgumentException if the JSON value is not a string
+     * @param json The JSON value to be deserialized.
+     * @return A `LocalDate` object.
+     * @throws IllegalArgumentException if the JSON value is not a string.
      */
     override def read(json: JsValue): LocalDate = json match {
       case JsString(value) => DateFormatterUtil.parseLocalDate(value)
-      case _ => throw new IllegalArgumentException(s"Expected a string, but received ${json.getClass.getSimpleName}.")
+      case _ => throw new IllegalArgumentException(s"Expected a string as a LocalDate, but received ${json.getClass.getSimpleName}.")
     }
   }
 
@@ -48,28 +74,26 @@ trait DefaultSerializeProtocol extends SprayJsonSupport {
    * Implicit JSON format for `LocalDateTime`.
    * Provides methods to serialize and deserialize `LocalDateTime` objects to and from JSON strings.
    */
-  implicit val localDateTimeFormat: RootJsonFormat[LocalDateTime] = new RootJsonFormat[LocalDateTime] {
+  implicit object LocalDateTimeJsonFormat extends JsonFormat[LocalDateTime] {
 
     /**
      * Serializes a `LocalDateTime` object to a JSON string.
      *
-     * @param dateTime The `LocalDateTime` object to be serialized
-     * @return A JSON string representing the `LocalDateTime` object
+     * @param obj The `LocalDateTime` object to be serialized.
+     * @return A JSON string representing the `LocalDateTime` object.
      */
-    override def write(dateTime: LocalDateTime): JsValue = {
-      JsString(DateFormatterUtil.formatLocalDateTime(dateTime))
-    }
+    override def write(obj: LocalDateTime): JsValue = JsString(DateFormatterUtil.formatLocalDateTime(obj))
 
     /**
      * Deserializes a JSON string to a `LocalDateTime` object.
      *
-     * @param json The JSON value to be deserialized
-     * @return A `LocalDateTime` object
-     * @throws IllegalArgumentException if the JSON value is not a string
+     * @param json The JSON value to be deserialized.
+     * @return A `LocalDateTime` object.
+     * @throws IllegalArgumentException if the JSON value is not a string.
      */
     override def read(json: JsValue): LocalDateTime = json match {
       case JsString(value) => DateFormatterUtil.parseLocalDateTime(value)
-      case _ => throw new IllegalArgumentException(s"Serialization Failure: Expected a string, but received ${json.getClass.getSimpleName}.")
+      case _ => throw new IllegalArgumentException(s"Serialization Failure: Expected a string as a LocalDateTime, but received ${json.getClass.getSimpleName}.")
     }
   }
 }
