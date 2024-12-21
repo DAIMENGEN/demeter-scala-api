cf763d849b0be1b5dd568ad953802da5badb95a7
mengen.dai <mengen.dai@outlook.com>
2024-12-21 23:49:09 +0800

    feat(core): 添加项目任务实体类
    - 创建 ProjectTaskEntity案类，包含任务属性和属性值
    - 实现 JSON 序列化和反序列化方法
    - 添加数据库表行工厂方法，支持创建和更新操作
    - 验证任务属性和属性值的数量关系

---
+++ src/main/scala/com/advantest/demeter/core/entity/project/task/ProjectTaskEntity.scala
@@ -0,0 +1,97 @@
+package com.advantest.demeter.core.entity.project.task
+
+import com.advantest.demeter.core.database.project.task.ProjectTaskDBTableRow
+import com.advantest.demeter.core.entity.project.task.attribute.ProjectTaskAttributeEntity
+import com.advantest.demeter.core.entity.project.task.value.ProjectTaskAttributeValueEntity
+import com.advantest.demeter.utils.database._
+import com.advantest.demeter.utils.serialize.{Serializable, format}
+import spray.json.DefaultJsonProtocol._
+import spray.json._
+
+import java.time.LocalDateTime
+
+/**
+ * Create on 2024/12/21
+ * Author: mengen.dai@outlook.com
+ */
+final case class ProjectTaskEntity(
+                                    id: Long,
+                                    taskName: String,
+                                    taskAttributes: Seq[ProjectTaskAttributeEntity] = Seq.empty,
+                                    taskAttributeValues: Seq[ProjectTaskAttributeValueEntity] = Seq.empty,
+                                    order: Option[Int] = None
+                                  ) {
+  // To support cases where attribute values can be empty, the lengths of taskAttributes and taskAttributeValues are validated.
+  assert(taskAttributes.size >= taskAttributeValues.size, "taskAttributes.size must bigger than taskAttributeValues.size.")
+
+  override def toString: String = s"ProjectTaskEntity(id=$id, taskName=$taskName, order=$order)"
+}
+
+object ProjectTaskEntity extends Serializable[ProjectTaskEntity] with DBTableRowFactory {
+  override protected type EntityData = ProjectTaskEntity
+  override protected type TableRowData = ProjectTaskDBTableRow
+
+  override implicit val serializeFormat: RootJsonFormat[ProjectTaskEntity] = new RootJsonFormat[ProjectTaskEntity] {
+    override def write(obj: ProjectTaskEntity): JsValue = {
+      val writer = format.writer.JsonWriterFormat()
+      val attributes = obj.taskAttributes.map(attribute => attribute.id -> attribute.taskAttributeName).toMap
+      obj.taskAttributeValues.collect {
+        case attributeValue: ProjectTaskAttributeValueEntity if attributes.contains(attributeValue.taskAttributeId) => attributes(attributeValue.taskAttributeId) -> attributeValue
+      }.foreach {
+        case (attributeName, attributeValue) =>
+          attributeValue.taskAttributeValue match {
+            case DBIntValue(value) => writer.writeInt(attributeName, value)
+            case DBLongValue(value) => writer.writeLong(attributeName, value)
+            case DBFloatValue(value) => writer.writeFloat(attributeName, value)
+            case DBDoubleValue(value) => writer.writeDouble(attributeName, value)
+            case DBStringValue(value) => writer.writeString(attributeName, value)
+            case DBTextValue(value) => writer.writeString(attributeName, value)
+            case DBMediumtextValue(value) => writer.writeString(attributeName, value)
+            case DBLongtextValue(value) => writer.writeString(attributeName, value)
+            case DBBooleanValue(value) => writer.writeBoolean(attributeName, value)
+            case DBJsonValue(value) => writer.writeString(attributeName, value.toString)
+            case DBDateValue(value) => writer.writeLocalDate(attributeName, value)
+            case DBDateTimeValue(value) => writer.writeLocalDateTime(attributeName, value)
+          }
+      }
+      writer.writeString("taskName", obj.taskName).writeInt("order", obj.order).toJsObject
+    }
+
+    override def read(json: JsValue): ProjectTaskEntity = {
+      json match {
+        case JsObject(fields) =>
+          val reader = format.reader.JsonReaderFormat(fields)
+          val id = reader.readLong("id")
+          val order = reader.readOptionInt("order")
+          val taskName = reader.readString("taskName")
+          val taskAttributes = reader.read[Seq[ProjectTaskAttributeEntity]]("taskAttributes")
+          val taskAttributeValues = reader.read[Seq[ProjectTaskAttributeValueEntity]]("taskAttributeValues")
+          ProjectTaskEntity(id, taskName, taskAttributes, taskAttributeValues, order)
+        case _ => deserializationError("ProjectTaskEntity deserialization error.")
+      }
+    }
+  }
+
+  override def create(employeeId: Long, entityData: EntityData, options: OptionalData = None): TableRowData = {
+    val maybeProjectId = options.flatMap(_.get("projectId").map(_.asInstanceOf[Long]))
+    maybeProjectId match {
+      case Some(projectId) =>
+        ProjectTaskDBTableRow(
+          id = entityData.id,
+          taskName = entityData.taskName,
+          order = entityData.order,
+          projectId = projectId,
+          creatorId = employeeId,
+          updaterId = employeeId,
+        )
+      case None => throw new IllegalArgumentException("ProjectId are required when creating a ProjectTaskDBTableRow")
+    }
+  }
+
+  override def update(employeeId: Long, entityData: EntityData, oldRowData: ProjectTaskDBTableRow, options: OptionalData = None): TableRowData = oldRowData.copy(
+    taskName = entityData.taskName,
+    order = entityData.order,
+    updaterId = employeeId,
+    updateDateTime = LocalDateTime.now()
+  )
+}
