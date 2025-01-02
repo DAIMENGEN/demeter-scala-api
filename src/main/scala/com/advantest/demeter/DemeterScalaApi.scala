ab7327097fe1bef0573e7d35fe2cd9c1e32096c8
mengen.dai <mengen.dai@outlook.com>
2025-01-02 09:14:38 +0800

    build(DemeterScalaApi):调整 JWT 配置加载位置
    - 将 JWT 配置加载相关代码从文件开头移动到数据库配置之后
    - 优化代码结构，提高可读性和维护性

--- src/main/scala/com/advantest/demeter/DemeterScalaApi.scala
+++ src/main/scala/com/advantest/demeter/DemeterScalaApi.scala
@@ -22,16 +22,6 @@ object DemeterScalaApi extends App {
   // Create an ExecutionContext for handling asynchronous operations.
   final lazy implicit val DEMETER_EXECUTION_CONTEXT: ExecutionContextExecutor = DEMETER_SYSTEM.executionContext
 
-  // Load configuration from the application.conf file.
-  private final lazy val DEMETER_CONFIG = ConfigFactory.load()
-  private final lazy val DEMETER_JWT_SECRETS = DEMETER_CONFIG.getList("akka.http.jwt.secrets")
-  private final lazy val DEMETER_JWT_SECRET_CONFIG = DEMETER_JWT_SECRETS.get(0).asInstanceOf[ConfigObject].toConfig
-  final lazy val DEMETER_JWT_ALGORITHM = pdi.jwt.JwtAlgorithm.HS256
-  final lazy val DEMETER_JWT_SECRET = DEMETER_JWT_SECRET_CONFIG.getString("secret")
-  final lazy val DEMETER_JWT_ISSUER = DEMETER_JWT_SECRET_CONFIG.getString("issuer")
-  final lazy val DEMETER_JWT_KEY_ID = DEMETER_JWT_SECRET_CONFIG.getString("key-id")
-
-
   // Configure the database using Slick.
   private lazy implicit val path: String = if (args.apply(0).equals("release")) {
     DEMETER_SYSTEM.log.info("Start with release environment.")
@@ -46,6 +36,15 @@ object DemeterScalaApi extends App {
   // Configure the database using Slick.
   val DATABASE_CONFIG: DatabaseConfig[JdbcProfile] = DatabaseConfig.forConfig[JdbcProfile](path)
   final lazy implicit val database: DATABASE_CONFIG.profile.backend.Database = DATABASE_CONFIG.db
+
+  // Load configuration from the application.conf file.
+  private final lazy val DEMETER_CONFIG = ConfigFactory.load()
+  private final lazy val DEMETER_JWT_SECRETS = DEMETER_CONFIG.getList("akka.http.jwt.secrets")
+  private final lazy val DEMETER_JWT_SECRET_CONFIG = DEMETER_JWT_SECRETS.get(0).asInstanceOf[ConfigObject].toConfig
+  final lazy val DEMETER_JWT_ALGORITHM = pdi.jwt.JwtAlgorithm.HS256
+  final lazy val DEMETER_JWT_SECRET = DEMETER_JWT_SECRET_CONFIG.getString("secret")
+  final lazy val DEMETER_JWT_ISSUER = DEMETER_JWT_SECRET_CONFIG.getString("issuer")
+  final lazy val DEMETER_JWT_KEY_ID = DEMETER_JWT_SECRET_CONFIG.getString("key-id")
 
   // Start Demeter Api Server.
   HttpRoute.start(HolidayRoute(), EmployeeRoute(), DepartmentRoute(), TeamRoute(), ProjectRoute())
